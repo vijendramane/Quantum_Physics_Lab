08c3f711604a940a5d97ec6358413839b40ba73b
Vijendra Mane <148106020+vijendramane@users.noreply.github.com>
2026-06-07 03:37:23 +0530

    Create bell_experiment_simulation.py

---
+++ 02_quantum_entanglement/bell_experiment_simulation.py
@@ -0,0 +1,21 @@
+import random
+
+trials = 1000
+
+same_results = 0
+
+for _ in range(trials):
+
+    particle_a = random.choice([0, 1])
+
+    particle_b = particle_a
+
+    if particle_a == particle_b:
+        same_results += 1
+
+print("Trials:", trials)
+
+print(
+    "Correlation:",
+    same_results / trials
+)
