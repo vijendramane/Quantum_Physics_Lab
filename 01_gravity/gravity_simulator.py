0d105b9fbc4b0a0123059f6328e99514a04429ba
Vijendra Mane <148106020+vijendramane@users.noreply.github.com>
2026-06-06 06:30:46 +0530

    Create gravity_simulator.py

---
+++ 01_gravity/gravity_simulator.py
@@ -0,0 +1,38 @@
+import math
+
+G = 6.67430e-11
+
+class CelestialBody:
+
+    def __init__(self, name, mass):
+        self.name = name
+        self.mass = mass
+
+def gravitational_force(body1, body2, distance):
+
+    return (
+        G *
+        body1.mass *
+        body2.mass /
+        distance**2
+    )
+
+earth = CelestialBody(
+    "Earth",
+    5.972e24
+)
+
+moon = CelestialBody(
+    "Moon",
+    7.348e22
+)
+
+distance = 384400000
+
+force = gravitational_force(
+    earth,
+    moon,
+    distance
+)
+
+print(f"Earth-Moon Force: {force:.2e} N")
