836af2d437fdfc1d0529dd615168aa800d41dd10
Vijendra Mane <148106020+vijendramane@users.noreply.github.com>
2026-06-11 00:41:24 +0530

    Create quantum_tunnel_simulation.py

---
+++ 03_quantum_tunneling/quantum_tunnel_simulation.py
@@ -0,0 +1,22 @@
+import random
+
+particles = 1000
+tunneling_probability = 0.15
+
+tunneled = 0
+
+for _ in range(particles):
+
+    if random.random() < tunneling_probability:
+        tunneled += 1
+
+blocked = particles - tunneled
+
+print("Total Particles:", particles)
+print("Tunneled:", tunneled)
+print("Blocked:", blocked)
+
+print(
+    "Observed Probability:",
+    tunneled / particles
+)
