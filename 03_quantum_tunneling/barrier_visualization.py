ada391a021dc0f73c4ad43e9e7b3408b81ff1cc6
Vijendra Mane <148106020+vijendramane@users.noreply.github.com>
2026-06-11 00:40:20 +0530

    Create barrier_visualization.py

---
+++ 03_quantum_tunneling/barrier_visualization.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.linspace(0, 10, 1000)
+
+barrier = np.where((x >= 4) & (x <= 6), 8, 0)
+
+particle_energy = np.ones_like(x) * 5
+
+plt.figure(figsize=(10, 5))
+plt.plot(x, barrier, label="Barrier Height")
+plt.plot(x, particle_energy, label="Particle Energy")
+
+plt.title("Quantum Tunneling Concept")
+plt.xlabel("Position")
+plt.ylabel("Energy")
+
+plt.legend()
+plt.grid(True)
+
+plt.show()
