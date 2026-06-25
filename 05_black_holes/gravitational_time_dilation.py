bda5da815f83248dd066a3c1b23195bf672e6136
Vijendra Mane <148106020+vijendramane@users.noreply.github.com>
2026-06-25 18:31:56 +0530

    Create gravitational_time_dilation.py

---
+++ 05_black_holes/gravitational_time_dilation.py
@@ -0,0 +1,29 @@
+"""
+Gravitational Time Dilation
+"""
+
+import math
+
+G = 6.67430e-11
+C = 299792458
+
+earth_mass = 5.972e24
+earth_radius = 6_371_000
+
+
+def time_dilation(mass, radius):
+
+    return math.sqrt(
+        1 -
+        (2 * G * mass) /
+        (radius * C ** 2)
+    )
+
+
+factor = time_dilation(
+    earth_mass,
+    earth_radius
+)
+
+print("Time Dilation Factor")
+print(factor)
