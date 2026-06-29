51c474131eefd3220bcf7ed6857abc897ecddaf6
Vijendra Mane <148106020+vijendramane@users.noreply.github.com>
2026-06-29 18:04:48 +0530

    Create star_light_curve.py

---
+++ 06_exoplanets/star_light_curve.py
@@ -0,0 +1,33 @@
+"""
+Star Light Curve
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+time = np.linspace(0, 50, 500)
+
+brightness = (
+    1
+    - 0.02 *
+    np.exp(
+        -((time - 25) ** 2) / 4
+    )
+)
+
+plt.plot(
+    time,
+    brightness
+)
+
+plt.title(
+    "Star Light Curve"
+)
+
+plt.xlabel("Time")
+
+plt.ylabel("Brightness")
+
+plt.grid()
+
+plt.show()
