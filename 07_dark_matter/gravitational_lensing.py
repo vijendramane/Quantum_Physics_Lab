0ab017c5737d3f0e762ce1b9b0234f4c62160260
Vijendra Mane <148106020+vijendramane@users.noreply.github.com>
2026-07-28 20:08:01 +0530

    Add gravitational lensing visualization script
    This script visualizes gravitational lensing using matplotlib.

---
+++ 07_dark_matter/gravitational_lensing.py
@@ -0,0 +1,28 @@
+"""
+Simple Gravitational Lensing Visualization
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+theta = np.linspace(0,2*np.pi,300)
+
+r = 1 + 0.15*np.sin(2*theta)
+
+x = r*np.cos(theta)
+y = r*np.sin(theta)
+
+plt.figure(figsize=(6,6))
+
+plt.plot(np.cos(theta),np.sin(theta),
+         '--',label="Original")
+
+plt.plot(x,y,label="Lensed")
+
+plt.axis("equal")
+
+plt.title("Gravitational Lensing")
+
+plt.legend()
+
+plt.show()
