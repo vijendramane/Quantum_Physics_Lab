ca695778dcbb87ce87f9d4af2657f6b9147f2bb3
Vijendra Mane <148106020+vijendramane@users.noreply.github.com>
2026-07-28 20:08:33 +0530

    Add galaxy mass estimator script

---
+++ 07_dark_matter/galaxy_mass_estimator.py
@@ -0,0 +1,17 @@
+"""
+Galaxy Mass Estimator
+"""
+
+G = 6.67430e-11
+
+velocity = 220000      # m/s
+
+radius = 50000 * 9.461e15
+
+mass = velocity**2 * radius / G
+
+solar_mass = 1.989e30
+
+print("Estimated Galaxy Mass")
+
+print(f"{mass/solar_mass:.2e} Solar Masses")
